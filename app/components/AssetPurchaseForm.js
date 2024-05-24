4af40d0b5d387de86e5819fa6abedb4548b224f7
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2024-05-24 03:22:00 +0000

    feat: fix DOM properties and pages

--- app/components/AssetPurchaseForm.js
+++ app/components/AssetPurchaseForm.js
@@ -13,6 +13,7 @@ const AssetPurchaseForm = ({ asset }) => {
 
     const handleBuy = async (e) => {
         e.preventDefault();
+
         if (quantity <= 0 || quantity > asset.value) {
             setError('Invalid quantity');
             return;
@@ -34,32 +35,21 @@ const AssetPurchaseForm = ({ asset }) => {
     };
 
     return (
-        
-        <div className="container mt-5">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
             <h2>Buy {asset.name}</h2>
             {error && <p className="text-danger">{error}</p>}
-            <form onSubmit={handleBuy}>
-                <div className="form-group">
-                    <label htmlFor="quantity">Quantity</label>
-                    <input
-                        type="number"
-                        id="quantity"
-                        className="form-control"
-                        value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                    />
+            <form class="max-w-sm mx-auto" onSubmit={handleBuy}>
+                <div class="mb-5">
+                    <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
+                    <input type="number" max={asset.remaining_supply} value={quantity} onChange={(e) => setQuantity(e.target.value)} id="quantity" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                 </div>
-                <div className="form-group form-check">
-                    <input
-                        type="checkbox"
-                        id="insuranceInclude"
-                        className="form-check-input"
-                        checked={insuranceInclude}
-                        onChange={(e) => setInsuranceInclude(e.target.checked)}
-                    />
-                    <label className="form-check-label" htmlFor="insuranceInclude">Include Insurance</label>
+                <div class="flex items-start mb-5">
+                    <div class="flex items-center h-5">
+                        <input id="insurance" type="checkbox" checked={insuranceInclude} onChange={(e) => setInsuranceInclude(e.target.checked)} value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+                    </div>
+                    <label for="insurance" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree to sign the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">insurance contract.</a></label>
                 </div>
-                <button type="submit" className="btn btn-success">Buy</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy Asset</button>
             </form>
         </div>
     );
--- app/components/AssetsTable.js
+++ app/components/AssetsTable.js
@@ -1,80 +1,79 @@
 import React from 'react';
 import Link from 'next/link';
 
-const AssetsTable = ({ data }) => (
-    <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-                Blockshield Assets
-                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Blockshield assets.</p>
-            </caption>
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Symbol
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Rating
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Origin
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Due Date
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Price
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Yield (%)
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        #
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) => (
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {item.name}
+const AssetsTable = ({ data }) => {
+    return (
+        <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                    Blockshield Assets
+                    <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Blockshield assets.</p>
+                </caption>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Name
                         </th>
-                        <td class="px-6 py-4">
-                            {item.symbol}
-                        </td>
-                        <td class="px-6 py-4">
-                            {item.rating}
-                        </td>
-                        <td class="px-6 py-4">
-                            {item.tokenization_platform}
-                        </td>
-                        <td class="px-6 py-4">
-                            {item.due_date}
-                        </td>
-                        <td class="px-6 py-4">
-                            {item.price.unitary_value} - {item.price.currency}
-                        </td>
-                        <td class="px-6 py-4">
-                            {item.price.yield}
-                        </td>
-                        <td class="px-6 py-4">
-                            <Link href={{ pathname: '/buy', query: { asset: JSON.stringify(item) } }}>
-                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    <svg class="w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+                        <th scope="col" className="px-6 py-3">
+                            Symbol
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Rating
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Origin
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Due Date
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Yield (%)
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            #
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {item.name}
+                            </th>
+                            <td className="px-6 py-4">
+                                {item.symbol}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.rating}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.tokenization_platform}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.due_date}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.price.unitary_value} - {item.price.currency}
+                            </td>
+                            <td className="px-6 py-4">
+                                {item.price.yield}
+                            </td>
+                            <td className="px-6 py-4">
+                                <Link href={{ pathname: '/buy', query: { asset: JSON.stringify(item) } }}>
+                                    <svg className="w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
                                         <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
                                     </svg>
-                                    Buy now
-                                </button>
-                            </Link>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-);
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 export default AssetsTable;
