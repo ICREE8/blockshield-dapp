422fa5a6f201b0621938e49002b90b9c45589c8c
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2024-06-02 05:15:22 +0000

    feat: add link to foward to the amoy polygon scan

--- app/components/SecuredAssetsTable.js
+++ app/components/SecuredAssetsTable.js
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link'
 
 const SecuredAssetsTable = ({ data }) => {
     // console.log("On sucured asset table....");
@@ -33,7 +34,17 @@ const SecuredAssetsTable = ({ data }) => {
                     {data.map((item, index) => (
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {item.transaction_hash}
+                                <Link 
+                                    href={{  pathname: `https://amoy.polygonscan.com/tx/${item.transaction_hash}` }} 
+                                    target="_blank"
+                                >
+                                    <p className="pointer-events-none text-blue-600 dark:text-blue-500">
+                                        {item.transaction_hash}
+                                        {/* <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg> */}
+                                    </p>
+                                </Link>
                             </th>
                             <td className="px-6 py-4">
                                 {item.symbol}
