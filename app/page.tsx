50b88328c300a579a307c821ade267d92cffeb25
Marduk <arenash8@gmail.com>
2024-10-12 22:05:27 -0400

    install dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ import HomePage from "./pages/index";
 
 export default function Home() {
   return (
-    <HomePage />
+    <div className="container">
+      <header className="header-box">
+        <div className="button-container">
+          <button className="rent-rv-button">Rent RV</button>
+          <button className="view-airport-button">View Airport</button>
+        </div>
+      </header>
+      <HomePage />
+    </div>
   );
 }
